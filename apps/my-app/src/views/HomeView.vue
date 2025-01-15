abe560bdbe0be242eb05770932a0d5fbfd80f1c5
mohammadSorooshfar <mohammad.sorooshfar@gmail.com>
2025-01-15 15:30:24 +0330

    feat: add component

--- apps/my-app/src/views/HomeView.vue
+++ apps/my-app/src/views/HomeView.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@org/my-lib';
+</script>
 
 <template>
   <main>
     <Button />
+    <v-btn>look</v-btn>
   </main>
 </template>
